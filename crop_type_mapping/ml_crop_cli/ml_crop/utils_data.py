f48f96d4f92b24d612d37d4f07bda54368e3fd58
Zhuangfang Yi <geospatialanalystyi@gmail.com>
2019-11-26 15:30:38 -0500

    resolve the git coflicts

--- crop_type_mapping/ml_crop_cli/ml_crop/utils_data.py
+++ crop_type_mapping/ml_crop_cli/ml_crop/utils_data.py
@@ -1,24 +1,11 @@
 import geopandas as gpd
-<<<<<<< HEAD
-=======
-# import matplotlib.pyplot as plt
->>>>>>> b74b3ee31dcad682359d4d1c628c348eed0c947a
 import numpy as np
 import rasterio
 from rasterio.features import rasterize
 from rasterstats.io import bounds_window
-<<<<<<< HEAD
 
 from sklearn.model_selection import train_test_split
 
-=======
-import rasterstats
-
-from sklearn.model_selection import train_test_split
-
-from os import path as op
-import pickle
->>>>>>> b74b3ee31dcad682359d4d1c628c348eed0c947a
 
 def crop_classes(train_geo):
     training_vectors = gpd.read_file(train_geo)
@@ -26,19 +13,12 @@ def crop_classes(train_geo):
     class_dict = dict(zip(classes, range(len(classes))))
     return class_dict
 
-<<<<<<< HEAD
 
 def all_values(x):
     return x
 
 
 def train_raw(raster_file, train_geo):
-=======
-def all_values(x):
-    return x
-
-def train_raw(raster_file, train_geo, bands):
->>>>>>> b74b3ee31dcad682359d4d1c628c348eed0c947a
     training_vectors = gpd.read_file(train_geo)
     class_dict = crop_classes(train_geo)
     print(class_dict)
@@ -47,23 +27,15 @@ def train_raw(raster_file, train_geo, bands):
     x_raw = []
     y_raw = []
     with rasterio.open(raster_file, 'r') as src:
-<<<<<<< HEAD
         for (label, geom) in zip(
                                 training_vectors.name,
                                 training_vectors.geometry):
-=======
-        for (label, geom) in zip(training_vectors.name, training_vectors.geometry):
->>>>>>> b74b3ee31dcad682359d4d1c628c348eed0c947a
             # read the raster data matching the geometry bounds
             window = bounds_window(geom.bounds, src.transform)
             # store our window information
             window_affine = src.window_transform(window)
             fsrc = src.read(window=window)
-<<<<<<< HEAD
             # rasterize the geometry into the larger shape and affine
-=======
-            # rasterize the (non-buffered) geometry into the larger shape and affine
->>>>>>> b74b3ee31dcad682359d4d1c628c348eed0c947a
             mask = rasterize(
                 [(geom, 1)],
                 out_shape=fsrc.shape[1:],
@@ -76,21 +48,13 @@ def train_raw(raster_file, train_geo, bands):
             label_pixels = np.argwhere(mask)
             for (row, col) in label_pixels:
                 # add a pixel of data to X
-<<<<<<< HEAD
                 data = fsrc[:, row, col]
                 one_x = np.nan_to_num(data, nan=1e-3)
                 x_raw.append(one_x)
-=======
-                data = fsrc[:,row,col]
-                one_x = np.nan_to_num(data, nan=1e-3)
-                x_raw.append(one_x)
-                # add the label to y
->>>>>>> b74b3ee31dcad682359d4d1c628c348eed0c947a
                 y_raw.append(class_dict[label])
 
     return x_raw, y_raw
 
-<<<<<<< HEAD
 
 def norm_inds(arr, a, b):
     inds = np.expand_dims((arr[..., a] - arr[..., b]) /
@@ -98,12 +62,6 @@ def norm_inds(arr, a, b):
     return inds
 
 
-=======
-def norm_inds(arr, a, b):
-    inds = np.expand_dims((arr[..., a] - arr[..., b])/(arr[..., a] + arr[..., b]), axis = 1)
-    return inds
-
->>>>>>> b74b3ee31dcad682359d4d1c628c348eed0c947a
 def train_split(X_raw, Y_raw):
     X = np.array(X_raw)
     y = np.array(Y_raw)
@@ -112,17 +70,10 @@ def train_split(X_raw, Y_raw):
     ndvi = norm_inds(X, 3, 2)
     ndwi = norm_inds(X, 1, 3)
 
-<<<<<<< HEAD
     X = np.concatenate([X, ndvi, ndwi], axis=1)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
     labels, counts = np.unique(y_train, return_counts=True)
-=======
-    X = np.concatenate([X, ndvi, ndwi], axis =1)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
-
-    labels, counts = np.unique(y_train, return_counts = True)
->>>>>>> b74b3ee31dcad682359d4d1c628c348eed0c947a
     class_weight_dict = dict(zip(labels, 1/counts))
 
     return X_train, X_test, y_train, y_test, class_weight_dict, labels
--- crop_type_mapping/ml_crop_cli/ml_crop/utils_train.py
+++ crop_type_mapping/ml_crop_cli/ml_crop/utils_train.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import pickle
 
 import lightgbm as lgb
@@ -14,25 +13,6 @@ def train(model_id, raster_file, train_geo, out_model):
     X_raw, Y_raw = train_raw(raster_file, train_geo)
     X_train, X_test, y_train, y_test, class_weight_dict, labels = train_split(X_raw, Y_raw)
     if model_id == "randomforest":
-=======
-# import numpy as np
-from sklearn.ensemble import RandomForestClassifier
-# from treeinterpreter import treeinterpreter as ti
-from sklearn.metrics import confusion_matrix
-from ml_crop.utils_data import train_raw, train_split, crop_classes
-
-import lightgbm as lgb
-
-from sklearn import svm
-
-import pickle
-
-def train(model_id, raster_file, train_geo, bands):
-    class_dict = crop_classes(train_geo)
-    X_raw, Y_raw = train_raw(raster_file, train_geo, bands)
-    X_train, X_test, y_train, y_test, class_weight_dict, labels = train_split(X_raw, Y_raw)
-    if model_id =="randomforest":
->>>>>>> b74b3ee31dcad682359d4d1c628c348eed0c947a
         model = RandomForestClassifier(
                 n_estimators=200,
                 class_weight=class_weight_dict,
@@ -41,7 +21,6 @@ def train(model_id, raster_file, train_geo, bands):
                 verbose=1,
                 random_state=0)
 
-<<<<<<< HEAD
     elif model_id == "lightgbm":
         model = lgb.LGBMClassifier(
                objective='multiclass',
@@ -64,24 +43,4 @@ def train(model_id, raster_file, train_geo, bands):
     print("*"*30)
     print("The model f1 score is {:0.2f}".format(f1))
     print("*"*30)
-=======
-    elif model_id =="lightgbm":
-        model = lgb.LGBMClassifier(
-               objective='multiclass',
-               class_weight = class_weight_dict,
-               num_class = len(class_dict),
-               metric = 'multi_logloss')
-
-    elif model_id == "svm":
-        model = svm.SVC(
-               class_weight = class_weight_dict,
-               gamma='scale',
-               decision_function_shape='ovo')
-    model.fit(X_train, y_train)
-    print("training {} model!".format(model_id))
-    preds = model.predict(X_test)
-    cm = confusion_matrix(y_test, preds, labels=labels)
-    print("model confusion_matrix is {}".format(cm))
-    print(cm)
->>>>>>> b74b3ee31dcad682359d4d1c628c348eed0c947a
     return model
