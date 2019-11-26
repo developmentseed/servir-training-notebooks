0bf9fd50e59765f7d3e5f0ea91e1ed3267eb457b
Zhuangfang Yi <geospatialanalystyi@gmail.com>
2019-11-26 14:05:31 -0500

    main CLI ml_crop

--- crop_type_mapping/ml_crop_cli/ml_crop/utils_predict.py
+++ crop_type_mapping/ml_crop_cli/ml_crop/utils_predict.py
@@ -1,76 +1,66 @@
-import numpy as np
-from sklearn.ensemble import RandomForestClassifier
-# from treeinterpreter import treeinterpreter as ti
-from sklearn.metrics import confusion_matrix
-from ml_crop.utils_data import train_raw, train_split, crop_classes, norm_inds
-from ml_crop.utils_train import train
-import rasterio
-import lightgbm as lgb
-
-from sklearn import svm
-
 import pickle
+import numpy as np
+import rasterio
+from ml_crop.utils_data import norm_inds
 
-def predict(model_id, raster_file,train_geo, bands, pred_tiff):
-    model = train(model_id, raster_file, train_geo, bands)
-    output_image = "{}_classification.tif".format(model_id)
-    src = rasterio.open(pred_tiff, 'r')
-    profile = src.profile
-    profile.update(
-        dtype=rasterio.uint8,
-        count=1,
-    )
-    dst = rasterio.open(output_image, 'w', **profile)
 
-    # perform prediction on each small image patch to minimize required memory
-    patch_size = 500
+def predict(out_model, pred_tiff, output_image):
+    with open(out_model, 'rb') as modelfile:
+        model = pickle.load(modelfile)
+    with rasterio.open(pred_tiff, 'r') as src:
+        profile = src.profile
+        profile.update(
+            dtype=rasterio.uint8,
+            count=1,
+        )
+        with rasterio.open(output_image, 'w', **profile) as dst:
 
-    for i in range((src.shape[0] // patch_size) + 1):
-        for j in range((src.shape[1] // patch_size) + 1):
-            # define the pixels to read (and write)
-            window = rasterio.windows.Window(
-                j * patch_size,
-                i * patch_size,
-                # don't read past the image bounds
-                min(patch_size, src.shape[1] - j * patch_size),
-                min(patch_size, src.shape[0] - i * patch_size)
-            )
+            # perform prediction on each small image patch to minimize required memory
+            patch_size = 500
 
-            data = src.read(window=window)
-            # read the image into the proper format, adding indices if necessary
-            img_swp = np.moveaxis(data, 0, 2)
-            img_flat = img_swp.reshape(-1, img_swp.shape[-1])
+            for i in range((src.shape[0] // patch_size) + 1):
+                for j in range((src.shape[1] // patch_size) + 1):
+                    # define the pixels to read (and write)
+                    window = rasterio.windows.Window(
+                        j * patch_size,
+                        i * patch_size,
+                        # don't read past the image bounds
+                        min(patch_size, src.shape[1] - j * patch_size),
+                        min(patch_size, src.shape[0] - i * patch_size)
+                    )
 
-            img_ndvi = norm_inds(img_flat, 3, 2)
-            img_ndwi = norm_inds(img_flat, 1, 3)
+                    data = src.read(window=window)
+                    # read the image into the proper format
+                    # adding indices if necessary
+                    img_swp = np.moveaxis(data, 0, 2)
+                    img_flat = img_swp.reshape(-1, img_swp.shape[-1])
 
-            img_w_ind = np.concatenate([img_flat, img_ndvi, img_ndwi], axis=1)
+                    img_ndvi = norm_inds(img_flat, 3, 2)
+                    img_ndwi = norm_inds(img_flat, 1, 3)
 
-            # remove no data values, store the indices for later use
-            # a later cell makes the assumption that all bands have identical no-data value arrangements
-            m = np.ma.masked_invalid(img_w_ind)
-            to_predict = img_w_ind[~m.mask].reshape(-1, img_w_ind.shape[-1])
+                    img_w_ind = np.concatenate([img_flat, img_ndvi, img_ndwi], axis=1)
 
-            # predict
-            if not len(to_predict):
-                continue
-            img_preds = model.predict(to_predict)
+                    # remove no data values, store the indices for later use
+                    # a later cell makes the assumption that
+                    # all bands have identical no-data value arrangements
+                    m = np.ma.masked_invalid(img_w_ind)
+                    to_predict = img_w_ind[~m.mask].reshape(-1, img_w_ind.shape[-1])
 
-            # add the prediction back to the valid pixels (using only the first band of the mask to decide on validity)
-            # resize to the original image dimensions
-            output = np.zeros(img_flat.shape[0])
-            output[~m.mask[:,0]] = img_preds.flatten()
-            output = output.reshape(*img_swp.shape[:-1])
+                    # predict
+                    if not len(to_predict):
+                        continue
+                    img_preds = model.predict(to_predict)
 
-            # create our final mask
-            mask = (~m.mask[:,0]).reshape(*img_swp.shape[:-1])
+                    # add the prediction back to the valid pixels
+                    # (using only the first band of the mask to decide on validity)
+                    output = np.zeros(img_flat.shape[0])
+                    output[~m.mask[:, 0]] = img_preds.flatten()
+                    # resize to the original image dimensions
+                    output = output.reshape(*img_swp.shape[:-1])
 
-            # write to the final file
-            dst.write(output.astype(rasterio.uint8), 1, window=window)
-            dst.write_mask(mask, window=window)
-            # write to the final file
-            dst.write(output.astype(rasterio.uint8), 1, window=window)
-            dst.write_mask(mask, window=window)
+                    # create our final mask
+                    mask = (~m.mask[:, 0]).reshape(*img_swp.shape[:-1])
 
-    src.close()
-    dst.close()
+                    # write to the final file
+                    dst.write(output.astype(rasterio.uint8), 1, window=window)
+                    dst.write_mask(mask, window=window)
--- crop_type_mapping/ml_crop_cli/ml_crop/utils_train.py
+++ crop_type_mapping/ml_crop_cli/ml_crop/utils_train.py
@@ -1,20 +1,18 @@
-# import numpy as np
-from sklearn.ensemble import RandomForestClassifier
-# from treeinterpreter import treeinterpreter as ti
-from sklearn.metrics import confusion_matrix
-from ml_crop.utils_data import train_raw, train_split, crop_classes
-
-import lightgbm as lgb
-
-from sklearn import svm
-
 import pickle
 
-def train(model_id, raster_file, train_geo, bands):
+import lightgbm as lgb
+from sklearn import svm
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import f1_score
+
+from ml_crop.utils_data import train_raw, train_split, crop_classes
+
+
+def train(model_id, raster_file, train_geo, out_model):
     class_dict = crop_classes(train_geo)
-    X_raw, Y_raw = train_raw(raster_file, train_geo, bands)
+    X_raw, Y_raw = train_raw(raster_file, train_geo)
     X_train, X_test, y_train, y_test, class_weight_dict, labels = train_split(X_raw, Y_raw)
-    if model_id =="randomforest":
+    if model_id == "randomforest":
         model = RandomForestClassifier(
                 n_estimators=200,
                 class_weight=class_weight_dict,
@@ -23,22 +21,26 @@ def train(model_id, raster_file, train_geo, bands):
                 verbose=1,
                 random_state=0)
 
-    elif model_id =="lightgbm":
+    elif model_id == "lightgbm":
         model = lgb.LGBMClassifier(
                objective='multiclass',
-               class_weight = class_weight_dict,
-               num_class = len(class_dict),
-               metric = 'multi_logloss')
+               class_weight=class_weight_dict,
+               num_class=len(class_dict),
+               metric='multi_logloss')
 
     elif model_id == "svm":
         model = svm.SVC(
-               class_weight = class_weight_dict,
+               class_weight=class_weight_dict,
                gamma='scale',
                decision_function_shape='ovo')
     model.fit(X_train, y_train)
+    with open(out_model, 'wb') as modelfile:
+        pickle.dump(model, modelfile)
     print("training {} model!".format(model_id))
     preds = model.predict(X_test)
-    cm = confusion_matrix(y_test, preds, labels=labels)
-    print("model confusion_matrix is {}".format(cm))
-    print(cm)
+    f1 = f1_score(y_test, preds, average='weighted')
+    # cm = confusion_matrix(y_test, preds, labels=labels)
+    print("*"*30)
+    print("The model f1 score is {:0.2f}".format(f1))
+    print("*"*30)
     return model
